4a692aa5d50a88a2bbf8cb4a95430f7186340c13
livankrekh <livannnn@yandex.ru>
2017-05-28 16:17:37 +0300

    go

--- include/lem.h
+++ include/lem.h
@@ -39,7 +39,9 @@ struct 			s_lem
 };
 
 int				test(t_lem *graph);
-void			get_path(t_lem *graph);
+int				**get_path(t_lem *graph);
 void			print_farm(t_lem *graph);
+void			lets_go(int **path, t_lem *graph);
+void			print_path(int **road, int p, t_lem *graph);
 
 #endif
